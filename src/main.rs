26f374583d9e56bb9117c6c291bfcfbe0efc763c
elblasco <64553614+elblasco@users.noreply.github.com>
2024-09-20 15:34:57 +0200

    del: bloat from main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod function;
+mod machine;
+mod tape;
 
-use crate::function::Function;
-use regex::Regex;
+use function::Function;
+
+use crate::machine::TuringMachine;
 
 #[derive(Debug)]
 pub enum MyErrors {
@@ -11,45 +14,15 @@ pub enum MyErrors {
 }
 
 fn main() -> Result<(), MyErrors> {
-    let input_file = std::env::args().nth(1);
+    let input_path = std::env::args().nth(1);
 
-    if input_file.is_none() {
+    if input_path.is_none() {
         return Err(MyErrors::NoFileProvided);
     }
 
-    let input_file = std::fs::read_to_string(input_file.unwrap());
+    let transition_function = Function::new(input_path.unwrap())?;
 
-    if input_file.is_err() {
-        return Err(MyErrors::OpeningFile);
-    }
-
-    let stringified_input: String = input_file.unwrap();
-
-    let parsed_input: Vec<String> = parse_input(stringified_input);
-
-    let transition_function: Function = Function::new();
+    println!("{}", transition_function);
 
     Ok(())
-}
-
-fn parse_input(input_file: String) -> Vec<String> {
-    let remove_comments_regex: Regex = Regex::new(r"\s*;.*").unwrap();
-
-    let input_matrix: Vec<&str> = input_file.split('\n').collect();
-    let mut input_matrix_string: Vec<String> = vec![];
-    for element_str in input_matrix {
-        input_matrix_string.push(element_str.to_string());
-    }
-
-    // Remove comments after a line of code
-    for elem in input_matrix_string.iter_mut() {
-        let found = remove_comments_regex.find(elem);
-        if let Some(match_struct) = found {
-            elem.drain(match_struct.range());
-        }
-    }
-
-    // Remove commet lines
-    input_matrix_string.retain(|row| !row.is_empty() && !remove_comments_regex.is_match(row));
-    input_matrix_string
 }
